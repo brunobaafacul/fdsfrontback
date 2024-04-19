50ce86167e937f12cc763fc42163ee34bde052f0
brunobaafacul <162391336+brunobaafacul@users.noreply.github.com>
2024-04-19 01:12:27 +0000

    v1

---
+++ demo/src/main/webapp/Main.java
@@ -0,0 +1,8 @@
+package br.pucrs.b.andrade001;
+
+public class Main {
+    public static void main(String[] args) {
+        AppCliente appCliente = new AppCliente();
+        appCliente.executar();
+        }
+}
